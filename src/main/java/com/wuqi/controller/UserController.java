c5a15c47732f813aff9c6554b62305ff0da8b372
wuqi <2532116344@qq.com>
2018-06-24 19:48:42 +0800

    feat：增加controller的解释

--- src/main/java/com/wuqi/controller/UserController.java
+++ src/main/java/com/wuqi/controller/UserController.java
@@ -3,8 +3,11 @@ package com.wuqi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+/**
+ * controller这里只是个摆设，我们只模拟对页面的访问，原理都一样
+ * @author wuqi
+ * @date 2018/06/24
+ */
 @Controller
-@RequestMapping("/user")
 public class UserController {
 }
